7926a490e973bd09d16797503c3aec4d2fc48af9
Graeme Wolfendale <gwolfendale@gmail.com>
2014-10-12 16:20:53 +0100

    Update floss.js

--- floss.js
+++ floss.js
@@ -1,7 +1,7 @@
 var titlex = 0;
 var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0) * 0.6,
 	height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) * 2;
-var margin = {top: 20, right: 20, bottom: 20, left: 84};
+var margin = {top: 20, right: 0, bottom: 20, left: 84};
     width = width - margin.right - margin.left;
     height = height - margin.top - margin.bottom;
 var i = 0,
